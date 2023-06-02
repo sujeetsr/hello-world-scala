fca396078c1a71ff446f2713cad43dd6cc68ccac
Sujeet Sreenivasan <sujeetsr@gmail.com>
2023-06-02 06:33:52 -0700

    fix: release tag name

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,12 +2,4 @@ import scala.io.StdIn.readLine
 import sys.process._
 
 @main def hello: Unit =
-  val tags = "git tag --list --sort=-version:refname --merged".!!
-  val tagList = tags.split("\\r?\\n").map(_.trim)
-  val tag = if tagList.length > 0 && tagList(0).length > 0 then tagList(0) else "0.0.0"
-  println(s"${tag}")
-
-
-
- 
-
+  println("Hello World")
